c297b7b5c0a6b547ea04682ed6f4cbe324cad183
Ivan <93859862+jshuckbot@users.noreply.github.com>
2023-05-26 18:47:19 +0700

    Improved blog (#5)
    ready Improved blog

--- blogapp/forms.py
+++ blogapp/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 
+from blogapp import models as blogapp_models
+
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = blogapp_models.Comment
+        fields = ["name", "email", "body"]

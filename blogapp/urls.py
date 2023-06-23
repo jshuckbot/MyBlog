92a1914e010a8a8d863481d688eb418f7086c969
jshuckbot <gorka_ivan@mail.ru>
2023-06-23 19:46:03 +0700

    add news feeds

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from blogapp import views
 from blogapp.apps import BlogappConfig
+from blogapp.feeds import LatestPostFeed
 
 app_name = BlogappConfig.name
 
@@ -12,4 +13,5 @@ urlpatterns = [
     path("<int:post_id>/share/", views.post_share, name="post_share"),
     path("<int:post_id>/comment/", views.post_comment, name="post_comment"),
     path("tag/<slug:tag_slug>/", views.post_list, name="post_list_by_tag"),
+    path("feed/", LatestPostFeed(), name="post_feed"),
 ]
